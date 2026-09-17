b2da64ac03f136adf36b344f73878e710b165aab
Gurmehtab <119009386+Gurmehtab1@users.noreply.github.com>
2026-09-17 10:10:53 +0530

    Time: 0 ms (100%), Space: 42.3 MB (66.26%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isHappy(int n) {
+        while(n!=1 && n!=4){
+            int sum=0;
+            while(n>0){
+                int digits=n%10;
+                sum=sum+digits*digits;
+                n=n/10;
+            }
+            n=sum;
+        }
+        if(n==1){
+            return true;
+        }
+
+        
+        return false;
+        
+        
+    }
+}
